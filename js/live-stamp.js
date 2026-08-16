7bf37463c7b782f75832cd165239dc2679307da9
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-16 16:18:44 -0400

    QA: 2026-08-16 Terminal windows should terminate after work is done (pass 4)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "63b7c64",
-  "hashFull": "63b7c64f9bccb5ac2543633a4f7fa41093986df8",
-  "subject": "QA: 2026-08-16 Terminal windows should terminate after work is done (pass 1)"
+  "hash": "e974761",
+  "hashFull": "e97476124bf225d124eb3421f4bba9df18fc1ac2",
+  "subject": "QA: 2026-08-16 Terminal windows should terminate after work is done (pass 4)"
 };
