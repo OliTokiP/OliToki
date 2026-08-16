243b99bfadcdc6737d889d0bcb5a4c21e04cdf6b
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-16 14:56:28 -0400

    QA: 2026-08-16 New bug form should have mobile layout (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "a56d09c",
-  "hashFull": "a56d09cacd758a65728909952acd227034a1b76d",
-  "subject": "QA: 2026-08-16 Ticketing Queue refinements (pass 1)"
+  "hash": "bea38ef",
+  "hashFull": "bea38ef9a15d0bb52592f06bac80fdc1f05e2bab",
+  "subject": "QA: 2026-08-16 New bug form should have mobile layout (pass 1)"
 };
