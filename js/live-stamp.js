5b834e4f686ccffa65aed92e98049f6a736979bd
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-16 14:56:36 -0400

    QA: 2026-08-16 Ticket form should include bug and feature request as checkboxes at the top (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "bea38ef",
-  "hashFull": "bea38ef9a15d0bb52592f06bac80fdc1f05e2bab",
-  "subject": "QA: 2026-08-16 New bug form should have mobile layout (pass 1)"
+  "hash": "0fc8b80",
+  "hashFull": "0fc8b80460b9cbccb3fbcd3fde2ea8c6f1c015d9",
+  "subject": "QA: 2026-08-16 Ticket form should include bug and feature request as checkboxes at the top (pass 1)"
 };
