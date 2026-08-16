cb13d3668701f341d04c5fbf0302531670b78a9e
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-16 15:10:34 -0400

    QA: 2026-08-16 Terminal windows should terminate after work is done (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "0fc8b80",
-  "hashFull": "0fc8b80460b9cbccb3fbcd3fde2ea8c6f1c015d9",
-  "subject": "QA: 2026-08-16 Ticket form should include bug and feature request as checkboxes at the top (pass 1)"
+  "hash": "63b7c64",
+  "hashFull": "63b7c64f9bccb5ac2543633a4f7fa41093986df8",
+  "subject": "QA: 2026-08-16 Terminal windows should terminate after work is done (pass 1)"
 };
