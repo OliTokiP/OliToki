8fb10b1295e6915776d20e7cf1f292d8da9db748
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-16 19:39:46 -0400

    QA: 2026-08-16 Zooming in on mobile view when editing fields in ticket (pass 3)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "2db7ebb",
-  "hashFull": "2db7ebbc79b7e8010ef568cf0b9e0810958c8d0c",
-  "subject": "QA: 2026-08-16 Bug log confirmation should include link back to Listener home page (pass 1)"
+  "hash": "210c94b",
+  "hashFull": "210c94b29c2edcf04f28a698d22db2c898b9f924",
+  "subject": "QA: 2026-08-16 Zooming in on mobile view when editing fields in ticket (pass 3)"
 };
