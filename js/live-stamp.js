79f13cc0d740d9d5c39a0bac2653b4833bb431ba
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-16 23:24:02 -0400

    QA: 2026-08-16 Put New Ticket button at the top of the page (pass 2)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "8e1c04a",
-  "hashFull": "8e1c04a27d5c9938cb32ee90cfdb4a9d25c5e934",
-  "subject": "QA: 2026-08-16 Put New Ticket button at the top of the page (pass 1)"
+  "hash": "7622f7f",
+  "hashFull": "7622f7f82716ee3c2212a149a4289df599cae885",
+  "subject": "QA: 2026-08-16 Put New Ticket button at the top of the page (pass 2)"
 };
