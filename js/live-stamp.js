a95e7287c588a440ed2739ba3fa3b5a5512c6afd
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-16 20:41:45 -0400

    QA: 2026-08-16 Add image file upload to tickets (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "c48eaba",
-  "hashFull": "c48eaba9ba7321c7180b3170952a6aea8d066e45",
-  "subject": "QA: 2026-08-16 Check-in; what's being driven by Google sheet (pass 1)"
+  "hash": "b7ea28d",
+  "hashFull": "b7ea28d9f04fc2677426a3c7938e985ddf37c962",
+  "subject": "QA: 2026-08-16 Add image file upload to tickets (pass 1)"
 };
