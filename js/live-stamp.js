68cc6d4debe07bda7bf88b0a79b57e8084517631
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-17 00:12:59 -0400

    QA: 2026-08-16 Ken Burns and Slideshow both broken (pass 2)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "964a46b",
-  "hashFull": "964a46b6670e67bf8d87b683c03a3acde4058f99",
-  "subject": "QA: 2026-08-16 Menu Manager should understand conditionals from sheet (pass 2)"
+  "hash": "c806853",
+  "hashFull": "c80685346242edefb99a25c0c8472ca68df9429d",
+  "subject": "QA: 2026-08-16 Ken Burns and Slideshow both broken (pass 2)"
 };
