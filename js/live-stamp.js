9080cca5d842f40945ca45b8bacf25f6f06a365a
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-17 16:08:59 -0400

    QA: 2026-08-17 Check Live buttons should open new tabs 2 (pass 2)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "3e46a63",
-  "hashFull": "3e46a63025808b26c67a0d8a920fbf3ced78a160",
-  "subject": "QA: 2026-08-17 Include in Listener default ON when making new tickets (pass 2)"
+  "hash": "7f635d2",
+  "hashFull": "7f635d214f943f2deb9964d562942d6e323ee47c",
+  "subject": "QA: 2026-08-17 Check Live buttons should open new tabs 2 (pass 2)"
 };
