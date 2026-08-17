f28b621290db4e7d5f8b2994c447a43a284b01b4
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-16 21:29:39 -0400

    QA: 2026-08-16 Testing to see if the image file uploaded worked. Respond very quickly with desc (pass 2)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "35258a3",
-  "hashFull": "35258a381628ee35695ed12fdcdb8500de3f5206",
-  "subject": "QA: 2026-08-16 Ticket titles being cutoff if too long (pass 1)"
+  "hash": "0c35a8e",
+  "hashFull": "0c35a8ef5c46210e0664900f00b87eea7a06a019",
+  "subject": "QA: 2026-08-16 Testing to see if the image file uploaded worked. Respond very quickly with desc (pass 2)"
 };
