fc1e176434dee73ebebf7918361fecdef3055489
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-16 23:26:54 -0400

    QA: 2026-08-16 Auto-refresh on ready for review (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "7622f7f",
-  "hashFull": "7622f7f82716ee3c2212a149a4289df599cae885",
-  "subject": "QA: 2026-08-16 Put New Ticket button at the top of the page (pass 2)"
+  "hash": "0b3bed4",
+  "hashFull": "0b3bed46229d0466130b7f2009c852c82f05c892",
+  "subject": "QA: 2026-08-16 Auto-refresh on ready for review (pass 1)"
 };
