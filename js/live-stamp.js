b125baebcfa2d5cebcf29f59fbbebd7d0efc796b
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-16 20:15:10 -0400

    QA: 2026-08-16 Change return link at the end of ticked logged page to match individual tickets. (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "210c94b",
-  "hashFull": "210c94b29c2edcf04f28a698d22db2c898b9f924",
-  "subject": "QA: 2026-08-16 Zooming in on mobile view when editing fields in ticket (pass 3)"
+  "hash": "645b746",
+  "hashFull": "645b7466d6fa3325773f2a1c5fe114826c063140",
+  "subject": "QA: 2026-08-16 Change return link at the end of ticked logged page to match individual tickets. (pass 1)"
 };
