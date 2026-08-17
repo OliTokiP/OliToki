eb9d896094263cdcdd176e1855bf6002c0e89bdd
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-17 14:16:13 -0400

    QA: 2026-08-17 Locked state for tickets that are in progress (pass 5)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "ac07ba2",
-  "hashFull": "ac07ba2253ab9f1c9d33c2e43ff43be8ee592a60",
+  "hash": "fc4329d",
+  "hashFull": "fc4329d1690e8b61df2d6899e143eaea71e9262a",
   "subject": "QA: 2026-08-17 Locked state for tickets that are in progress (pass 5)"
 };
