7614faf8b34c82dea2bbb9d0b9270e0007edcbe4
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-17 16:10:40 -0400

    QA: 2026-08-17 Include in Listener default ON when making new tickets (pass 4)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "1f27aec",
-  "hashFull": "1f27aec485b66f1815d69c22ebbc3ebf0d4e897b",
-  "subject": "QA: 2026-08-17 Include in Listener default ON when making new tickets (pass 3)"
+  "hash": "71d8619",
+  "hashFull": "71d8619e889f237078b5602060ff731f8efa77c2",
+  "subject": "QA: 2026-08-17 Include in Listener default ON when making new tickets (pass 4)"
 };
