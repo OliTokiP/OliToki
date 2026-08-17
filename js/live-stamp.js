535a00aa4c6a6a49e7c93a2494d417c476cb3b0f
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-16 21:56:26 -0400

    QA: 2026-08-16 Ticket type selector (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "66a31ae",
-  "hashFull": "66a31aef08341001d70d5bd1a35dd129c6859c89",
-  "subject": "QA: 2026-08-16 Add image file upload to tickets (pass 2)"
+  "hash": "6d571ff",
+  "hashFull": "6d571ff3b39fc1763707b043ccc4743c9df273a2",
+  "subject": "QA: 2026-08-16 Ticket type selector (pass 1)"
 };
