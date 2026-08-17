96cecfc7f4015ba88b77f2afc5a203504b35ac7d
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-17 19:24:30 -0400

    QA: 2026-08-17 ALL NEW TICKETS MARKED AS DRAFT (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "6998705",
-  "hashFull": "69987053b388f8079f6ae8094e9b9c9a0e04e004",
-  "subject": "QA: 2026-08-17 Under Version in system settings, include latest git commit comment (pass 2)"
+  "hash": "c0bf254",
+  "hashFull": "c0bf2547cf285666144a03c960b0d0ea1250b9eb",
+  "subject": "QA: 2026-08-17 ALL NEW TICKETS MARKED AS DRAFT (pass 1)"
 };
