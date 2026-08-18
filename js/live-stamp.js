b5b0a6b0abb068e80233da573120c4dfa376dbf0
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-17 22:42:26 -0400

    individual menu editors added

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "8e219ae",
-  "hashFull": "8e219ae10bc1e663364f0f982e4f1e6eaae66dce",
-  "subject": "QA: 2026-08-17 Text shrinks ever so slightly when the user taps the font selector, regardless of whether they actually pick something different. (pass 1)"
+  "hash": "de1822b",
+  "hashFull": "de1822b827d6622165dfee5edf7fb14a65afc6d7",
+  "subject": "individual menu editors added"
 };
