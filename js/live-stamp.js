3141f9eebca173c0bf40724aa570464a0b41f79d
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-18 10:54:55 -0400

    QA: 2026-08-18 Piggyback tickets not behaving as expected (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "50fd79e",
-  "hashFull": "50fd79e54d40764f32114b8eda23d98fabf4b390",
-  "subject": "QA: 2026-08-18 Animated ... to toggle switches on dashboard (pass 2)"
+  "hash": "27d6219",
+  "hashFull": "27d6219a6c7981c2a1f3f3bf9bbf3edf6979b88a",
+  "subject": "QA: 2026-08-18 Piggyback tickets not behaving as expected (pass 1)"
 };
