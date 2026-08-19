b51df2df23793ef06c351ae2aa078687211f5ac1
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-19 19:12:43 -0400

    QA: 2026-08-19 Ticket view - Special formatting for ticket navigation (pass 2)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "23c746c",
-  "hashFull": "23c746c585c7566ae405edcfd8985311e92d0c54",
-  "subject": "QA: 2026-08-19 Ticket view - Special formatting for ticket navigation (pass 1)"
+  "hash": "f9de60c",
+  "hashFull": "f9de60c6593c179cb440ec331afdbc4fd2c00c07",
+  "subject": "QA: 2026-08-19 Ticket view - Special formatting for ticket navigation (pass 2)"
 };
