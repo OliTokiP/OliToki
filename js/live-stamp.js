80aed95359681ba96e68436643588d12ba908d12
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-19 12:26:50 -0400

    QA: 2026-08-19 Tapping Listener toggles (crawl, onoff) should replace their text with a small animation and become un-touchable until state has changed (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "b624ba3",
-  "hashFull": "b624ba3a7a108034baa42aed66c21ef6cbfcd51a",
-  "subject": "QA: 2026-08-19 Favorites queue should get Crawl button (pass 1)"
+  "hash": "984bd15",
+  "hashFull": "984bd1559a3bcb2984c15f6847f915c469b95b05",
+  "subject": "QA: 2026-08-19 Tapping Listener toggles (crawl, onoff) should replace their text with a small animation and become un-touchable until state has changed (pass 1)"
 };
