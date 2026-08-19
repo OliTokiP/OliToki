6ae52d83d6a9be23033c1d15bc24805895e95cd2
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-19 19:13:30 -0400

    QA: 2026-08-19 Give Paste function to Ticket Reporting (pass 3)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "f9de60c",
-  "hashFull": "f9de60c6593c179cb440ec331afdbc4fd2c00c07",
-  "subject": "QA: 2026-08-19 Ticket view - Special formatting for ticket navigation (pass 2)"
+  "hash": "b799cc0",
+  "hashFull": "b799cc0c52e4017592e6ce997883abffd2570d49",
+  "subject": "QA: 2026-08-19 Give Paste function to Ticket Reporting (pass 3)"
 };
