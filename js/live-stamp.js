d438a93582d6529c8bd5d7e9a25a761fd58a1852
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-18 23:31:11 -0400

    QA: 2026-08-18 System Font does not update menu boards (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "988d81e",
-  "hashFull": "988d81e0ee4daf9a2ace7540e1e6bfe77d3822e1",
-  "subject": "QA: 2026-08-18 Board names revert on Menu Settings (pass 1)"
+  "hash": "ef8a516",
+  "hashFull": "ef8a516fa6e3d1a9446410490800f6d78dfde0d1",
+  "subject": "QA: 2026-08-18 System Font does not update menu boards (pass 1)"
 };
