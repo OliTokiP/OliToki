69793b7f0128b3ad543262683ea95cf73a94a281
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-19 18:32:25 -0400

    QA: 2026-08-19 More Tooltips for refactor + Save Stack behavior (pass 2)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "6d0c861",
-  "hashFull": "6d0c86102ed43fde5f2340e9f2ecf53e6902fa55",
-  "subject": "QA: 2026-08-19 Pill location determination unclear in queues (pass 5)"
+  "hash": "f3d8bab",
+  "hashFull": "f3d8bab5533e827d1859db0b7fb78dc37eb67110",
+  "subject": "QA: 2026-08-19 More Tooltips for refactor + Save Stack behavior (pass 2)"
 };
