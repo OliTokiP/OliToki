f33c64ebb3a3344efa4e40e9159969b422fe357d
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-19 18:02:03 -0400

    QA: 2026-08-19 Better Attachment Management (pass 3)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "2105840",
-  "hashFull": "2105840cc77d61ca27a9a24ae28e0c36c1c986e6",
-  "subject": "QA: 2026-08-19 Ticket should should inherit links to surfaces in scope in Ticket view (pass 1)"
+  "hash": "6c833d4",
+  "hashFull": "6c833d4b17e1b45f4d5f521ef9290913bb23b29d",
+  "subject": "QA: 2026-08-19 Better Attachment Management (pass 3)"
 };
