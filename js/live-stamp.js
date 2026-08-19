b584a60b0c62039ed831328e7407b56897b1ef25
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-19 18:52:59 -0400

    QA: 2026-08-19 Drafts and piggyback tickets should show filled but greyed out type and surface options at the top (pass 3)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "1cf687e",
-  "hashFull": "1cf687ed3c7a132ed0f33b8983d95f9fc93b471c",
-  "subject": "QA: 2026-08-19 Give Paste function to Ticket Reporting (pass 1)"
+  "hash": "11c8e87",
+  "hashFull": "11c8e8772993d35561b7dfa941e0e2c09b64f4eb",
+  "subject": "QA: 2026-08-19 Drafts and piggyback tickets should show filled but greyed out type and surface options at the top (pass 3)"
 };
