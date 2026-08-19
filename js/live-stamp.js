41a3ef5afd922837ead59160b1ad55013e894e47
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-19 15:57:24 -0400

    QA: 2026-08-19 Pill location determination unclear in queues (pass 2)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "ebf6994",
-  "hashFull": "ebf6994fa69952fd3656e01e182e43723c73b02d",
+  "hash": "9b31aa2",
+  "hashFull": "9b31aa260ebf8836357bd356982809df76076212",
   "subject": "QA: 2026-08-19 Pill location determination unclear in queues (pass 2)"
 };
