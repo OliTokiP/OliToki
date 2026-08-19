df172522d8e7332c1b79135f6888a19c33d775b0
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-19 12:50:05 -0400

    QA: 2026-08-19 Tapping Listener toggles (crawl, onoff) should replace their text with a small animation and become un-touchable until state has changed (pass 2)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "3b1226e",
-  "hashFull": "3b1226ec52e8c0d104d54f35adfcee11642dc5cc",
-  "subject": "QA: 2026-08-19 Expand confirm save toggle in preferences (pass 2)"
+  "hash": "12b3db2",
+  "hashFull": "12b3db27538c76e24f0bfd7dcff066e3e4c9240c",
+  "subject": "QA: 2026-08-19 Tapping Listener toggles (crawl, onoff) should replace their text with a small animation and become un-touchable until state has changed (pass 2)"
 };
