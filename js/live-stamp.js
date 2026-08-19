f8ee5dc44a29d394181291df97aadc770b46b4ea
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-19 19:53:01 -0400

    QA: 2026-08-19 Ticketer doesn't have have attachment management (pass 4)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "0934a59",
-  "hashFull": "0934a592c50e0e3e4c2127e17371ede1cf5afd5e",
-  "subject": "QA: 2026-08-19 Ticketer doesn't have have attachment management (pass 3)"
+  "hash": "3be540c",
+  "hashFull": "3be540c46da46a6064ebe9ebe61e3c3f429f5334",
+  "subject": "QA: 2026-08-19 Ticketer doesn't have have attachment management (pass 4)"
 };
