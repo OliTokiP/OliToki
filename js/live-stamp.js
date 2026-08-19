3a61ce5571dfbf6bde7643733e321db78f7fce50
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-19 19:38:01 -0400

    QA: 2026-08-19 Ticket View Title should copy ticket link to clipboard (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "1476e87",
-  "hashFull": "1476e8789e5a0bf1051f18595d9b9b2b7cd31113",
-  "subject": "QA: 2026-08-19 Ticketer doesn't have have attachment management (pass 2)"
+  "hash": "aca6ced",
+  "hashFull": "aca6ced90ffaa3f092b1a9b3d586061138f94a9c",
+  "subject": "QA: 2026-08-19 Ticket View Title should copy ticket link to clipboard (pass 1)"
 };
