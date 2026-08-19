d6275a4a065cf78f7c85328085ea7b7e11d65113
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-18 20:11:53 -0400

    QA: restore Film/Galaxy sm webps dropped in deployer-surface commit

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "58209b9",
-  "hashFull": "58209b930a54ff4db0f99225c64cd9a422763aef",
-  "subject": "QA: 2026-08-18 Add deployer as surface to Listener (and all that entails) (pass 1)"
+  "hash": "2f26a48",
+  "hashFull": "2f26a48ad7d02a0ea4198bafbae74911fdc875b5",
+  "subject": "QA: restore Film/Galaxy sm webps dropped in deployer-surface commit"
 };
