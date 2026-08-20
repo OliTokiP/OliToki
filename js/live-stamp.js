f39a427b2dee66dd7b27cd1e86499813608b7e40
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-20 18:04:24 -0400

    QA: 2026-08-20 New sticker is a little crowded when using Popins font (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "d197e4a",
-  "hashFull": "d197e4a3fd0993c66e5cca5eb5d3f7fcb8249918",
-  "subject": "QA: 2026-08-20 Presentation style shouldn't be in Style and Theme - it is local to boards (pass 1)"
+  "hash": "b70b014",
+  "hashFull": "b70b014c175cbe723e5fc439298f427680f470c9",
+  "subject": "QA: 2026-08-20 New sticker is a little crowded when using Popins font (pass 1)"
 };
