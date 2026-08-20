6fbbd0a06639250f58550c9b2c465a0be0681a76
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-19 23:46:51 -0400

    QA: 2026-08-19 Wallpaper glitched when changing speed to 0 with Require restart turned off (pass 1)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "08e5861",
-  "hashFull": "08e5861765839a7a323f0bcb65582d41170a2bc5",
-  "subject": "QA: 2026-08-19 Encore Hard (Shadow) presentation style has incorrect shrink timing (pass 2)"
+  "hash": "d34b39d",
+  "hashFull": "d34b39d4ed689764090cc10f6051d071cdbfd866",
+  "subject": "QA: 2026-08-19 Wallpaper glitched when changing speed to 0 with Require restart turned off (pass 1)"
 };
