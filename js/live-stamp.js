572b3aef9d7d7e19403746d9a6f292fcd45210b6
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-20 19:03:10 -0400

    QA: 2026-08-20 Ticket Ready For Review badge should navigate to ticket page when clicked (pass 3)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "debe1b2",
-  "hashFull": "debe1b23e3542b17d1bdb1498f01dd657e0bf086",
-  "subject": "QA: 2026-08-20 Ticket Ready For Review badge should navigate to ticket page when clicked (pass 2)"
+  "hash": "cdf4af6",
+  "hashFull": "cdf4af6bbee0ac07f760a6b0e3d568719b90489d",
+  "subject": "QA: 2026-08-20 Ticket Ready For Review badge should navigate to ticket page when clicked (pass 3)"
 };
