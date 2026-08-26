abe15ad26aaef4c37ec44e1ae91f5066e851ef55
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-08-26 01:55:55 -0400

    Restore Film/Galaxy -sm webp (unrelated to item editor pass 4)

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "71d12bf",
-  "hashFull": "71d12bfaa43ef890d664bce5f84c85afbbad09d2",
-  "subject": "QA: 2026-08-25 Item Uploader incorporated into beta version of menu manager (pass 4)"
+  "hash": "01a2388",
+  "hashFull": "01a23885a904570615e6075f68daf2626b9ddefa",
+  "subject": "Restore Film/Galaxy -sm webp (unrelated to item editor pass 4)"
 };
