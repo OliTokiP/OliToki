1fe4f404f78e2ec04d4ba220b919d9c0fb26d7d0
OliTokiP <317138617+OliTokiP@users.noreply.github.com>
2026-09-24 13:21:14 +0000

    deploy: pin restaurant env (restaurant) from cc91c1a

--- js/live-stamp.js
+++ js/live-stamp.js
@@ -1,5 +1,5 @@
 window.TOKI_LIVE_STAMP = {
-  "hash": "7f63666",
-  "hashFull": "7f63666cf28f655ddd14854526f875c2d6b6dff1",
+  "hash": "cc91c1a",
+  "hashFull": "cc91c1a48756f9b30d5fc9fc990fd035da0f7149",
   "subject": "QA: 2026-09-22 Closed Status Feature (pass 2)"
 };
